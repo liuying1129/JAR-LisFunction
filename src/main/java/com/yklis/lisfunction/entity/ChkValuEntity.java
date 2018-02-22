08c24d6d008999a74cc0e366da7801805df68a94
liuying1129 <liuying1129@163.com>
2018-02-22 15:52:53 +0800

    base64方式显示图片

--- src/main/java/com/yklis/lisfunction/entity/ChkValuEntity.java
+++ src/main/java/com/yklis/lisfunction/entity/ChkValuEntity.java
@@ -4,6 +4,10 @@ public class ChkValuEntity {
 
     private String tableName;//chk_valu、chk_valu_bak
     private int valueid;
+    private int pkunid;
+    private String name;
+    private String english_name;
+    private String issure;
     private byte[] photo;
     
     public String getTableName() {
@@ -20,6 +24,38 @@ public class ChkValuEntity {
 
     public void setValueid(int valueid) {
         this.valueid = valueid;
+    }
+    
+    public int getPkunid() {
+        return this.pkunid;
+    }
+
+    public void setPkunid(int pkunid) {
+        this.pkunid = pkunid;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEnglish_name() {
+        return this.english_name;
+    }
+
+    public void setEnglish_name(String english_name) {
+        this.english_name = english_name;
+    }
+        
+    public String getIssure() {
+        return this.issure;
+    }
+
+    public void setIssure(String issure) {
+        this.issure = issure;
     }
 
     public byte[] getPhoto() {
